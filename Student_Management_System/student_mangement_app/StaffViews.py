16794d506c8ffb91dbad4727c6b56b822c58c679
md rafiqul islam <76397310+rafiqul0396@users.noreply.github.com>
2021-04-04 19:47:19 +0530

    my Projects

---
+++ Student_Management_System/student_mangement_app/StaffViews.py
@@ -0,0 +1,135 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage #To upload Profile Picture
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+import json
+
+
+from student_management_app.models import CustomUser, Staffs, Courses, Subjects, Students, SessionYearModel, Attendance, AttendanceReport, LeaveReportStaff, FeedBackStaffs, StudentResult
+
+
+def staff_home(request):
+    # Fetching All Students under Staff
+
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    course_id_list = []
+    for subject in subjects:
+        course = Courses.objects.get(id=subject.course_id.id)
+        course_id_list.append(course.id)
+    
+    final_course = []
+    # Removing Duplicate Course Id
+    for course_id in course_id_list:
+        if course_id not in final_course:
+            final_course.append(course_id)
+    
+    students_count = Students.objects.filter(course_id__in=final_course).count()
+    subject_count = subjects.count()
+
+    # Fetch All Attendance Count
+    attendance_count = Attendance.objects.filter(subject_id__in=subjects).count()
+    # Fetch All Approve Leave
+    staff = Staffs.objects.get(admin=request.user.id)
+    leave_count = LeaveReportStaff.objects.filter(staff_id=staff.id, leave_status=1).count()
+
+    #Fetch Attendance Data by Subjects
+    subject_list = []
+    attendance_list = []
+    for subject in subjects:
+        attendance_count1 = Attendance.objects.filter(subject_id=subject.id).count()
+        subject_list.append(subject.subject_name)
+        attendance_list.append(attendance_count1)
+
+    students_attendance = Students.objects.filter(course_id__in=final_course)
+    student_list = []
+    student_list_attendance_present = []
+    student_list_attendance_absent = []
+    for student in students_attendance:
+        attendance_present_count = AttendanceReport.objects.filter(status=True, student_id=student.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(status=False, student_id=student.id).count()
+        student_list.append(student.admin.first_name+" "+ student.admin.last_name)
+        student_list_attendance_present.append(attendance_present_count)
+        student_list_attendance_absent.append(attendance_absent_count)
+
+    context={
+        "students_count": students_count,
+        "attendance_count": attendance_count,
+        "leave_count": leave_count,
+        "subject_count": subject_count,
+        "subject_list": subject_list,
+        "attendance_list": attendance_list,
+        "student_list": student_list,
+        "attendance_present_list": student_list_attendance_present,
+        "attendance_absent_list": student_list_attendance_absent
+    }
+    return render(request, "staff_template/staff_home_template.html", context)
+
+
+
+def staff_take_attendance(request):
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    session_years = SessionYearModel.objects.all()
+    context = {
+        "subjects": subjects,
+        "session_years": session_years
+    }
+    return render(request, "staff_template/take_attendance_template.html", context)
+
+
+def staff_apply_leave(request):
+    staff_obj = Staffs.objects.get(admin=request.user.id)
+    leave_data = LeaveReportStaff.objects.filter(staff_id=staff_obj)
+    context = {
+        "leave_data": leave_data
+    }
+    return render(request, "staff_template/staff_apply_leave_template.html", context)
+
+
+def staff_apply_leave_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('staff_apply_leave')
+    else:
+        leave_date = request.POST.get('leave_date')
+        leave_message = request.POST.get('leave_message')
+
+        staff_obj = Staffs.objects.get(admin=request.user.id)
+        try:
+            leave_report = LeaveReportStaff(staff_id=staff_obj, leave_date=leave_date, leave_message=leave_message, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Applied for Leave.")
+            return redirect('staff_apply_leave')
+        except:
+            messages.error(request, "Failed to Apply Leave")
+            return redirect('staff_apply_leave')
+
+
+def staff_feedback(request):
+    staff_obj = Staffs.objects.get(admin=request.user.id)
+    feedback_data = FeedBackStaffs.objects.filter(staff_id=staff_obj)
+    context = {
+        "feedback_data":feedback_data
+    }
+    return render(request, "staff_template/staff_feedback_template.html", context)
+
+
+def staff_feedback_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method.")
+        return redirect('staff_feedback')
+    else:
+        feedback = request.POST.get('feedback_message')
+        staff_obj = Staffs.objects.get(admin=request.user.id)
+
+        try:
+            add_feedback = FeedBackStaffs(staff_id=staff_obj, feedback=feedback, feedback_reply="")
+            add_feedback.save()
+            messages.success(request, "Feedback Sent.")
+            return redirect('staff_feedback')
+        except:
+            messages.error(request, "Failed to Send Feedback.")
+            return redirect('staff_feedback')
+
